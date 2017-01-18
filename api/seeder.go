2221d8685aea0692f9412adc02ef8b9804a6474a
William Dussault <dalloriam@gmail.com>
2017-01-18 08:59:03 -0500

    Seeder + Endpoint will (#8)
    * Fix circle deploy
    
    * Update secret
    
    * Fix circle build
    
    * Fix circle for real pls
    
    * seeder
    
    * Fix API structure
    
    * Add endpoint, seeding, models, interrupts, exception handling
    
    * Repo structure
    
    * Update imports

---
+++ api/seeder.go
@@ -0,0 +1,43 @@
+package api
+
+import (
+	"encoding/json"
+
+	"net/http"
+
+	"github.com/TSAP-Laval/common"
+)
+
+// SeedHandler gère l'endpoint de seeding
+func (c *ConsumeService) SeedHandler(w http.ResponseWriter, r *http.Request) {
+
+	dbtype := c.config.DatabaseDriver
+	connstring := c.config.ConnectionString
+
+	go c.seedData(dbtype, connstring)
+
+	bytes, err := json.Marshal(simpleMessage{Body: "Seeding started."})
+
+	if err != nil {
+		c.logger.Printf("ERROR - %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		c.ErrorWrite("An error occured", w)
+	}
+
+	_, err = w.Write(bytes)
+
+	if err != nil {
+		c.logger.Printf("ERROR - %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+}
+
+func (c *ConsumeService) seedData(dbType string, connString string) {
+	c.Info("Seeding...")
+	err := common.SeedData(c.config.DatabaseDriver, c.config.ConnectionString, c.config.SeedDataPath)
+	if err != nil {
+		c.Error(err.Error())
+	} else {
+		c.Info("Seeding complete.")
+	}
+}
