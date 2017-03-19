279144903403fbd2ae439167c26da5b5fd010191
William Dussault <dalloriam@gmail.com>
2017-03-19 12:04:33 -0400

    Add metric description

--- app/modules/metricsmodule/metrics_schemas.go
+++ app/modules/metricsmodule/metrics_schemas.go
@@ -3,6 +3,7 @@ package metricsmodule
 // MetricsCreationSchema représente le JSON posté vers
 // le serveur lors de la création d'une métrique
 type MetricsCreationSchema struct {
-	Name    string `json:"nom"`
-	Formula string `json:"equation"`
+	Name        string `json:"name"`
+	Formula     string `json:"formula"`
+	Description string `json:"description"`
 }
